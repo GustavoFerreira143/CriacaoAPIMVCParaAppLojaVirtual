7ef8f26818ff41ced510eba1fd965d28bb48efd9
Gustavo Ferreira de Oliveira Coelho <gustavo.feneita1@gmail.com>
2025-03-21 10:13:11 -0300

    Inicio do Projeto

---
+++ Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
+{
+    serverOptions.Listen(System.Net.IPAddress.Parse("192.168.100.63"), 5098);  // Pode mudar a porta se necessário
+});
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Este é o Inicio da Api para APP do RestShopVT</h1>
+<hr/>
+<p>Este projeto teve inicio em 21/03/2025</p>
